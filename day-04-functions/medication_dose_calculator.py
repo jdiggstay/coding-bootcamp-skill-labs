f9199840fc57742c11a1a72ddce410f65e2cbb17
JALISA DIGGS-TAYLOR <jalisadiggs-taylor@Mac.lan>
2026-05-16 19:38:19 -0500

    Complete Day 4 functions and medication dose calculator

---
+++ day-04-functions/medication_dose_calculator.py
@@ -0,0 +1,13 @@
+print("Medication Dose Calculator")
+
+def calculate_dose(weight_kg, dose_mg_per_kg):
+    return weight_kg * dose_mg_per_kg
+
+
+weight = float(input("Enter patient weight in kg: "))
+dose_per_kg = float(input("Enter prescribed dose (mg/kg): "))
+
+total_dose = calculate_dose(weight, dose_per_kg)
+
+print()
+print("Total dose: {total_dose:.2f} mg")
